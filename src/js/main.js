4b1d8ca7c48ee3fe1739b583dbeb29ab2cd198d8
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-05-20 12:17:01 +0200

    add print pokemon detail functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,9 @@
 
 const inputElement = document.querySelector(".js-input");
 const btnSearch = document.querySelector(".js-searchBtn");
-
 const searchPrevent = document.querySelector(".js-form");
+const pokemonDetail = document.querySelector(".js-pokemonDetail");
+
 function handleForm(ev) {
   ev.preventDefault();
 }
@@ -17,21 +18,39 @@ const getDataFromApi = (pokemon) => {
     .then((data) => {
       const pokemon = {
         pokemonName: data.name,
-        image: data.sprites.front_default,
+        image: data.sprites.other["official-artwork"].front_default,
         type: data.types.map((type) => type.type.name),
-        imageType: data.types.map((type) => type.type.url),
         weight: data.weight,
         height: data.height,
       };
-
+      console.log(data);
       return pokemon;
     });
 };
 
 //Botón buscar
 
+const paintDetails = (pokemon) => {
+  let htmlCode = "";
+  htmlCode += `<h3 class="pokemonDetail__title">${pokemon.pokemonName}</h3>`;
+  htmlCode += `<img class="pokemonDetail__img" src="${pokemon.image}">`;
+  htmlCode += `<h3 class="pokemonDetail__type">Type:</h3>`;
+  for (let i = 0; i < pokemon.type.length; i++) {
+    htmlCode += `<p class="pokemonDetail__type--title""> ${pokemon.type[i]}</p>`;
+  }
+  htmlCode += `<p class="pokemonDetail__height">Height:</p>`;
+  htmlCode += `<p class="pokemonDetail__weight">${pokemon.height}"</p>`;
+  htmlCode += `<p class="pokemonDetail__weight">Weight:</p>`;
+  htmlCode += `<p class="pokemonDetail__weight">${pokemon.weight} lbs</p>`;
+  pokemonDetail.innerHTML = htmlCode;
+};
+let pokemon = {};
+
 const handleSearchInput = () => {
-  getDataFromApi(inputElement.value).then((x) => console.log(x));
+  getDataFromApi(inputElement.value).then((x) => {
+    pokemon = x;
+    paintDetails(x);
+  });
 };
 
 btnSearch.addEventListener("click", handleSearchInput);
